6a9d2c63b5d37ddf78ebe3d61e31b21992be61c5
barunranjan <vrnranjan22@gmail.conm>
2022-03-22 11:31:04 +0530

    goal controller with route and error handler setup done

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,36 +1,46 @@
-
+const asyncHandler = require('express-async-handler')
 /**
  * @description GET goals
  * @route GET /api/goals
  * @access Private
  */
-const getGoals = (req, res)=>{
+const getGoals = asyncHandler(async(req, res)=>{
     res.status(200).json({message:"get the goal"})
-}
+})
 /**
  * @description POST goals
  * @route GET /api/goals
  * @access Private
  */
- const createGoal = (req, res)=>{
+ const createGoal = asyncHandler(async(req, res)=>{
+     if(!req.body.text){
+         /**
+          * default error handler
+          */
+        // res.status(400).json({message:"Please add a valid text"})
+
+        res.status(400)
+        throw new Error("Please add valid text")
+
+     }
     res.status(200).json({message:"set the goal"})
-}
+})
 /**
  * @description PUT goals
  * @route PUT /api/goals/:id
  * @access Private
  */
- const updateGoal = (req, res)=>{
+ const updateGoal = asyncHandler(async(req, res)=>{
     res.status(200).json({message:`update ${req.params.id} the goal`})
-}
+})
 /**
  * @description DELETE goals
  * @route DELETE /api/goals/:id
  * @access Private
  */
- const deleteGoal = (req, res)=>{
+ const deleteGoal = asyncHandler(async(req, res)=>{
     res.status(200).json({message:`delete ${req.params.id} goal`})
-}
+})
 
 module.exports={
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express =  require('express')
 const dotEnv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+
+/**
+ * middle ware needed to parse the body. now body parser comes inbuilt with express
+ */
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port,()=>console.log(`server up and running at ${port}`))
