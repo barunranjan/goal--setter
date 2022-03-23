91d14d82e2d8f233519f3efc9b7034e967d42ef2
barunranjan <vrnranjan22@gmail.conm>
2022-03-23 16:57:04 +0530

    added authorization

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,13 +1,49 @@
-
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler  = require('express-async-handler')
+const User = require('../models/userModel')
 /**
  * @description Register new user
  * @route POST /api/users
  * @access Public
  */
 
-const registerUser = (req, res)=>{
-    res.json({message:"register user"})
-}
+const registerUser = asyncHandler(async(req, res)=>{
+    const {email, name, password}=req.body
+    if(!email||!name||!password){
+        res.status(400)
+        throw new Error("Please add all field")
+    }
+
+    //check if user exist
+
+    const userExist = await User.findOne({email})
+    if(userExist){
+        res.status(400)
+        throw new Error('User already exist')
+    }
+    //Hash the password
+    
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //create user
+
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+    if(user){
+        res.status(200).json({
+            _id:user._id,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
 
 
 /**
@@ -16,9 +52,20 @@ const registerUser = (req, res)=>{
  * @access Public
  */
 
- const loginUser = (req, res)=>{
-    res.json({message:"Login user"})
-}
+ const loginUser = asyncHandler(async(req, res)=>{
+     const {email, password} = req.body
+     const user = await User.findOne({email})
+     if(user&&(await bcrypt.compare(password,user.password))){
+         res.json({
+             _id:user.id,
+             name:user.name,
+             token:generateToken(user._id)
+         })
+     }else{
+         res.status(400)
+         throw new Error('Invalid credentials')
+     }
+})
 
 
 
@@ -28,8 +75,18 @@ const registerUser = (req, res)=>{
  * @access Public
  */
 
- const getMe = (req, res)=>{
+ const getMe = asyncHandler((req, res)=>{
     res.json({message:"user data"})
+})
+
+/**
+ * Generate Token
+ */
+
+const generateToken = (id)=>{
+   return jwt.sign({id}, process.env.JWT_SECRET,{
+       expiresIn:'30d'
+   }) 
 }
 
 
