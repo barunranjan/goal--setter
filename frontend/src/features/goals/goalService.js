700f475d73abf941b5c6b7c985ca4ccbd05737bd
barunranjan <vrnranjan22@gmail.conm>
2022-04-08 11:36:34 +0530

    application complete

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/goals";
+const API_URL = "http://localhost:5000/api/goals/";
 
 const createGoal = async (goalData, token) => {
   const config = {
@@ -9,12 +9,33 @@ const createGoal = async (goalData, token) => {
     },
   };
   const response = await axios.post(API_URL, goalData, config);
-  console.log(response);
+  return response.data;
+};
+
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(API_URL, config);
+  return response.data;
+};
+
+const deleteGoal = async (goalId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.delete(API_URL + goalId, config);
   return response.data;
 };
 
 const goalService = {
   createGoal,
+  getGoals,
+  deleteGoal,
 };
 
 export default goalService;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,33 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const { user } = useSelector((state) => state.auth);
+
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
     if (!user) {
       navigate("/login");
     }
-  }, [navigate, user]);
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
 
   return (
     <>
@@ -21,17 +38,17 @@ const Dashboard = () => {
 
       <GoalForm />
 
-      {/* <section className='content'>
-      {goals.length > 0 ? (
-        <div className='goals'>
-          {goals.map((goal) => (
-            <GoalItem key={goal._id} goal={goal} />
-          ))}
-        </div>
-      ) : (
-        <h3>You have not set any goals</h3>
-      )}
-    </section> */}
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
     </>
   );
 };
